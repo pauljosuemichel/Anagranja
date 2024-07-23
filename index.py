b2c7a5dd7b8583a4dd17d7033ef478d4d4a961cb
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2024-07-23 16:50:08 -0300

    pagina

---
+++ index.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    return render_template('Home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3500)
